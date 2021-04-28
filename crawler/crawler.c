4d19d5c9bfafc8d8bc2873703ba8ec255341bb40
= <amittaijoel@outlook.com>
2021-04-28 17:44:43 -0400

    started crawler

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -21,17 +21,17 @@ int parseArgs(const char* argv);
 int 
 main(int argc, const char* argv[])
 {
-    /* code */
-    parseArgs(argv);
-    return 0;
+  /* code */
+  parseArgs(argv);
+  return 0;
 }
 
 int
 parseArgs(const char* argv)
 {
-    char* seedDirectory = argv[1];
-    char* pageDirectory = argv[2];
-    char* maxDepth = argv[3];
-    
+  char* seedDirectory = argv[1];
+  char* pageDirectory = argv[2];
+  char* maxDepth = argv[3];
+
 }
 
