50154fbb2e5d0a2aa416829313960f6eaa7c7d5d
= <amittaijoel@outlook.com>
2021-05-02 04:34:44 -0400

    Finalized tests

--- crawler/testing.sh
+++ crawler/testing.sh
@@ -21,13 +21,28 @@
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/output/letters-6
 
 # four args (too many)
-./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/output/letters-6 6 dummy-arg
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/output/letters-6 6 dummy-arg
 
 # three args (maxDepth invalid)
-./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/output/letters-10 -1
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/output/letters-10 -1
 
-# three args (valid)
-./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/output/letters-10 10
 
-# three args (valid)
-./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/ ../data/output/wikipedia-3 3
+# VALID TESTS:
+
+# letters, maxDepth = 0
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/output/letters-0 0
+
+# letters, maxDepth = 10
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/output/letters-10 10
+
+# toscrape, maxDepth = 0
+./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/index.html ../data/output/toscrape-0 0
+
+# toscrape, maxDepth = 1
+./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/index.html ../data/output/toscrape-1 1
+
+# wikipedia, maxDepth = 0
+./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/index.html ../data/output/wikipedia-0 0
+
+# wikipedia, maxDepth = 1
+./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/index.html ../data/output/wikipedia-1 1
---
+++ crawler/valgrind.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# valgrind.sh
+# 
+# bash script to run valgrind tests on the crawler
+# usage:
+#   ./testing.sh
+# output:
+#   stdout
+#
+# Amittai Wekesa, April 2021
+
+
+# for memory-leak tests
+VALGRIND=(valgrind --leak-check=full --show-leak-kinds=all)
+
+printf "\n\n\n"
+# letters, maxDepth = 0
+"${VALGRIND[@]}" ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/output/letters-0 0
+
+printf "\n\n\n"
+# letters, maxDepth = 10
+"${VALGRIND[@]}" ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/output/letters-10 10
+
+printf "\n\n\n"
+# toscrape, maxDepth = 0
+"${VALGRIND[@]}" ./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/index.html ../data/output/toscrape-0 0
+
+printf "\n\n\n"
+# toscrape, maxDepth = 1
+"${VALGRIND[@]}" ./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/index.html ../data/output/toscrape-1 1
+
+printf "\n\n\n"
+# wikipedia, maxDepth = 0
+"${VALGRIND[@]}" ./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/index.html ../data/output/wikipedia-0 0
+
+printf "\n\n\n"
+# wikipedia, maxDepth = 1
+"${VALGRIND[@]}" ./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/index.html ../data/output/wikipedia-1 1
