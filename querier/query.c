5b4e7c0656fba7f92aa636d4dd09d0065edd0b74
= <amittaijoel@outlook.com>
2021-05-18 06:42:28 -0400

    updated query functionality

--- querier/query.c
+++ querier/query.c
@@ -1,7 +1,8 @@
 /**
  * @file query.c
  * @author Amittai J.Wekesa (@siavava)
- * @brief 
+ * @brief: This file contains definition and functionality 
+ * of a struct to handle back-end functionality for the TSE querier
  * @version 0.1
  * @date 2021-05-13
  * 
@@ -9,21 +10,27 @@
  * 
  */
 
+/* standard libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 
+/* webpage handler */
 #include "webpage.h"
 
+/* data types */
 #include "index.h"
 #include "counters.h"
 
-#include "query.h"
+/* file handler */
+#include "file.h"
 
+/* helper library */
 #include "pagedir.h"
 
-#include "file.h"
+/*     self     */
+#include "query.h"
 
 
 /*********** Function Prototypes *********/
@@ -31,7 +38,11 @@ static void dubCounters(void* arg, int docID, int count);
 static void intersect(void* arg, int docID, int count);
 static void unite(void* arg, int docID, int count);
 static void sort(void* arg, int docID, int count);
+static void setZero(void* arg, int docID, int count);
 
+
+/* global constants */
+static const int MAXPAGES = 200;
 
 
 typedef struct query {
@@ -43,12 +54,20 @@ typedef struct query {
   counters_t* ctrs;
 } query_t;
 
+/*
+ * getter method
+ */
 counters_t*
 query_getCounters(query_t* query)
 {
   return query->ctrs;
 }
 
+/**
+ * @brief Constructor
+ * 
+ * @return query_t*: a pointer to a  query_t object
+ */
 query_t*
 query_new()
 {
@@ -64,8 +83,10 @@ query_new()
   query->numWords = 0;
   query->numPages = 0;
 
-  /* initialize pageDirectory to NULL */
+  /* initialize struct pointers to NULL */
+  query->docIDs = NULL;
   query->pageDirectory = NULL;
+  query->pages = NULL;
 
   /* initialize counters */
   query->ctrs = counters_new();
@@ -74,46 +95,90 @@ query_new()
   return query;
 }
 
+/**
+ * @brief deconstructor
+ * 
+ * @param query: pointer to a valid query_t object
+ */
 void 
 query_delete(query_t* query)
 {
   if (query != NULL) {
-    // if (query->ctrs != NULL) {
-    //   counters_delete(query->ctrs);
-    // }
-    // if (query->docIDs != NULL) {
-    //   mem_free(query->docIDs);
-    // }
-    // if (query->pages != NULL) {
-    //   mem_free(query->pages);
-    // }
+    if (query->ctrs != NULL) {
+      counters_delete(query->ctrs);
+    }
+    if (query->docIDs != NULL) {
+      mem_free(query->docIDs);
+    }
+    if (query->pages != NULL) {
+      for (int i=0; ; i++) {
+        if (query->pages[i] == NULL) {
+          break;
+        }
+        webpage_delete(query->pages[i]);
+      }
+      mem_free(query->pages);
+    }
     mem_free(query);
   }
 }
 
+/**
+ * @brief: assembles the results of a specific word's occurrence in the index.
+ * Returns NULL in case of error or non-existence.
+ * Also frees the passed in word. 
+ * 
+ * @param index: pointer to valid index object.
+ * @param subQuery: the sequence of words to be checked in the index.
+ * @return query_t*: a query struct containing the results.
+ */
 query_t*
-query_build(index_t* index, char** words)
+query_build(index_t* index, char** subQuery)
 {
-  assert(index != NULL && words != NULL);
+  if (index != NULL && subQuery != NULL) {
+    /* create query struct */
+    query_t* query = query_new();
 
-  /* create query struct */
-  query_t* query = query_new();
+    if (query != NULL) {
+      int i = 0;                                    // track index in words
+      char* word = NULL;                            // track current word
+      while( (word = subQuery[i]) != NULL) {
 
-  int i = 0;                                    // track index in words
-  char* word = NULL;                            // track current word
-  while( (word = words[i]) != NULL) {
+        // find the intersection with next word
+        query_intersection(index, query, word);
 
-    // find the intersection with next word
-    query_intersection(index, query, word);
+        // free word
+        mem_free(word);
 
-    // increment index
-    i++;
+        // step to next index
+        i++;
+      }
+
+      /* free the subQ */
+      mem_free(subQuery);
+
+      /* return generated query struct */
+      return query;
+    }
+
+    /* free the subQ */
+    mem_free(subQuery);
+
+    /* error creating query -- return NULL */
+    return NULL;
   }
 
-  /* return generated query struct */
-  return query;
+  /* index is NULL or subQ is NULL; return NULL */
+  return NULL;  
 }
 
+/**
+ * @brief: calculates the intersection of two queries in the index.
+ * 
+ * @param index: a pointer to a valid index_t* object.
+ * @param query: pointer to a valid query_t* object
+ * @param nextWord: next word to check and intersect with current results in the query.
+ */
 void 
 query_intersection(index_t* index, query_t* query, char* nextWord) {
 
@@ -122,18 +187,14 @@ query_intersection(index_t* index, query_t* query, char* nextWord) {
 
     /* find counters of word */
     counters_t* nextCounts = index_find(index, nextWord);
-    printf("Next counts: %s\n", nextWord);
-    counters_print(nextCounts, stdout);
-    printf("\n");
 
     /* 
      * if word does not exist in page (NULL return),
-     * set counters in query to NULL.
+     * set all counts in query to zero
      */
     if (nextCounts == NULL) {
-      // if (query->ctrs != NULL) {
-      //   counters_delete(query->ctrs);
-      // }
+      /* set all in query to 0 */
+      counters_iterate(query->ctrs, query->ctrs, setZero);
       query->numWords++;
     }
 
@@ -143,9 +204,6 @@ query_intersection(index_t* index, query_t* query, char* nextWord) {
      */
     else if (query->numWords == 0) {
 
-      /* find counters of word */
-      counters_t* nextCounts = index_find(index, nextWord);
-
       /* copy the counters */
       counters_iterate(nextCounts, query->ctrs, dubCounters);
 
@@ -153,37 +211,37 @@ query_intersection(index_t* index, query_t* query, char* nextWord) {
       query->numWords++;
     }
     else {
-      /* find the counters of next word */
-      counters_t* nextCounts = index_find(index, nextWord);
-      if (nextCounts != NULL) {
 
-        /* create counters of intersection */
-        counters_t* intersection = counters_new();
+      counters_t** buffer = mem_malloc(sizeof(query->ctrs) + sizeof(nextCounts));
+      counters_t* intersection = counters_new();
+      buffer[0] = nextCounts;
+      buffer[1] = intersection;
 
-        assert(intersection != NULL);
+      counters_iterate(query->ctrs, buffer, intersect);
+      
+      counters_delete(query->ctrs);
+      query->ctrs = intersection;
 
-        /* copy counts inside query to intersection */
-        counters_iterate(query->ctrs, intersection, dubCounters);
-
-        /* iterate over counters of next word, checking for commonality */
-        counters_iterate(nextCounts, intersection, intersect);
-
-        /* delete current counters in query */
-        counters_delete(query->ctrs);
-
-        /* save generated counters of intersections to the query */
-        query->ctrs = intersection;
-        query->numWords++;
-      }
+      query->numWords++;
     }
   }
 }
 
+/**
+ * @brief: function to build a union of the results of two subqueries.
+ * This function also deletes the input subqueries after computing their union.
+ * 
+ * @param subQuery1: pointer to a query struct.
+ * @param subQuery2: pointer to a query struct.
+ * @return query_t*: a query struct whose results constitute a union of the input subqueries.
+ */
 query_t*
 query_union(query_t* subQuery1, query_t* subQuery2)
 {
   /* assert all parameters are valid */
-  assert( (subQuery1 != NULL) && (subQuery2 != NULL) );
+  if (subQuery1 == NULL || subQuery2 == NULL) {
+    return NULL;
+  }
 
   /* 
    * create query struct for the union
@@ -191,7 +249,8 @@ query_union(query_t* subQuery1, query_t* subQuery2)
    */
   query_t* query;
   if ( (query = query_new()) == NULL) {
-    /* delete the two original sub queries being */
+    /* delete the two original sub queries */
+    fprintf(stderr, "Error creating query for union.");
     query_delete(subQuery1);
     query_delete(subQuery2);
     return NULL;
@@ -209,66 +268,59 @@ query_union(query_t* subQuery1, query_t* subQuery2)
   /* iterate over counters in second query */
   counters_iterate(subQuery2->ctrs, query->ctrs, unite);
 
-  /* delete the two original sub queries being */
+  /* delete the two original sub queries */
   query_delete(subQuery1);
   query_delete(subQuery2);
 
   /* return unioned query */
-  printf("UNIONED:\n");
-  counters_print(query->ctrs, stdout);
   return query;
 }
 
+/**
+ * @brief: merge the query results with data from the index and pages.
+ * 
+ * @param query: pointer to a query struct.
+ * @param pageDirectory: file directory wherein pages are saved.
+ */
 void
 query_index(query_t* query, char* pageDirectory)
 {
-  printf("START QUERY INDEX\n");
-  counters_print(query->ctrs, stdout);
-  printf("\n");
+  if (query == NULL || query->ctrs == NULL) {
+    return;
+  }
+
   /* buffer to hold sorted docID-value pairs */
-  int** buffer = mem_calloc_assert(2, 200*sizeof(int), "Error allocating memory in query_sort.");
+  int** buffer = mem_calloc_assert(2, 200*sizeof(int), "Error allocating memory in query_index.");
 
   buffer[0] = mem_calloc(100, sizeof(int));
   buffer[1] = mem_calloc(100, sizeof(int));
 
-  query->pages = mem_malloc_assert(200, "Error allocating memory in query_sort");
+  char* sampleURL = mem_calloc(50, sizeof(char));
+  strcpy(sampleURL, "sampleURL.com");
+  webpage_t* samplePage = webpage_new(sampleURL, 100, NULL);
 
-  query->docIDs = mem_calloc_assert(200, sizeof(int), "Error allocating memory in query_sort.");
+  query->pages = mem_calloc_assert(200, sizeof(samplePage), "Error allocating memory in query_index");
 
-  /* initialize all values to ZERO */
-  for (int i=0; i<100; i++) {   // assumption: max num of pages = 100
-    buffer[0][i] = 0;           // to hold keys
-    buffer[1][i] = 0;           // to hold values
-  }
+  webpage_delete(samplePage);
+
+  query->docIDs = mem_calloc_assert(200, sizeof(int), "Error allocating memory in query_index.");
 
   /* iterate through counters to sort calues. */
-  counters_iterate(query->ctrs, buffer, sort);
-
-  printf("FINISH SORTING INDEX\n");
-  for (int i=0; i<100; i++) {   // assumption: max num of pages = 100
-    printf("%d ", buffer[0][i]);           // to hold keys
+  if (query->ctrs != NULL) {
+    counters_iterate(query->ctrs, buffer, sort);
   }
-  printf("\n");
-  for (int i=0; i<100; i++) {   // assumption: max num of pages = 100
-    printf("%d ", buffer[1][i]);           // to hold keys
-  }
-  printf("\n");
-
-  
-  counters_print(query->ctrs, stdout);
-  printf("\n");
 
   /* create counters of sorted values */
   counters_t* sorted = counters_new();
   int* keys = buffer[0];
   int* counts = buffer[1];
 
-  /* index pages, find last index */
-  int lastIndex=0;
+  /* index pages */
   for (int i=0; ; i++) {
 
-    /* at ZERO key (end of items), break the loop. */
+    /* at ZERO key (end of items), save it and break the loop. */
     if (keys[i] == 0) {
+      query->numPages = i;
       break;
     }
 
@@ -280,52 +332,68 @@ query_index(query_t* query, char* pageDirectory)
     else {
       sprintf(filepath, "%s/%d", pageDirectory, keys[i]);
     }
-    printf("%s\n", filepath);
     webpage_t* page = pagedir_load(filepath);
+    
+    // query->pages = NULL;
     query->pages[i] = page;
     query->docIDs[i] = keys[i];
-    lastIndex = i;
   }
-
-  /* save last index + 1 to be total number of pages */
-  query->numPages = lastIndex + 1;
 
   /* add in values in the new order */
-  for (int i=lastIndex; i>=0; i--) {
+  for (int i=0; i<query->numPages; i++) {
     counters_set(sorted, keys[i], counts[i]);
   }
-  printf("SORTED INDEX\n");
-  counters_print(sorted, stdout);
-  printf("\n");
-
-  /* free the buffer */
-  mem_free(buffer);
 
   /* delete old counters */
   counters_delete(query->ctrs);
 
   /* save new counters */
   query->ctrs = sorted;
+
+  /* free the buffer */
+  mem_free(keys);
+  mem_free(counts);
+  mem_free(buffer);
 }
 
+/**
+ * @brief: prints the results held in a query struct.
+ * 
+ * @param query: pointer to a valid query struct
+ * @param fp: pointer to a file open for wtiring (could be stdout).
+ */
 void
 query_print(query_t* query, FILE* fp)
 {
   if (query != NULL && fp != NULL) {
     webpage_t** pages = query->pages;
     int* docIDs = query->docIDs;
-    assert(pages != NULL && docIDs != NULL);
-    fprintf(fp, "Matches %d documents (ranked):\n", query->numPages);
-    for (int i = 0; i<query->numPages; i++) {
-      int docID = docIDs[i];
-      int score = counters_get(query->ctrs, docID);
-      char* url = webpage_getURL(pages[i]);
-      fprintf(fp, "score  %d doc %d: %s\n", score, docID, url);
+    if (pages != NULL && docIDs != NULL) {
+      if (query->numPages == 0) {
+        fprintf(fp, "Matches %d documents.\n", query->numPages);
+      }
+      else{
+        fprintf(fp, "Matches %d documents (ranked):\n", query->numPages);
+        for (int i = 0; i<query->numPages; i++) {
+          int docID = docIDs[i];
+          if (docID > 0) {
+            int score = counters_get(query->ctrs, docID);
+            char* url = webpage_getURL(pages[i]);
+            fprintf(fp, "score  %d doc %d: %s\n", score, docID, url);
+          }
+        }
+      }
     }
   }
 }
 
-
+/**
+ * @brief: sorts a duo of arrays containing key-value pairs
+ * 
+ * @param arg: pointer to two coupled arrays.
+ * @param docID: document ID (key)
+ * @param count: count
+ */
 static void
 sort(void* arg, int docID, int count)
 {
@@ -339,18 +407,18 @@ sort(void* arg, int docID, int count)
     /* find size of array */
     int size = 0;
     for (int i=0; ; i++) {
-      if (keys[i] == 0) {
+      if (counts[i] == 0) {
         break;
       }
       size = i+1;
     }
 
     /* loop through array to find point where values fit in. */
-    for (int i=0; i<=size; i++) {
+    for (int i=0; i<=size+1; i++) {
       if (count > counts[i]) {
 
         /* shift values after index by 1 to the right. */
-        for (int j=size-1; j>i; j--) {
+        for (int j=size+1; j>i; j--) {
           keys[j] = keys[j-1];
           counts[j] = counts[j-1];
         }
@@ -358,11 +426,21 @@ sort(void* arg, int docID, int count)
         /* save in the values */
         keys[i] = docID;
         counts[i] = count;
+        break;
       }
     }
   }
 }
 
+/**
+ * @brief: create a union of two counters sets.
+ * receives key-value pairs from one counters set
+ * and compounds them into the other counters set.
+ * 
+ * @param arg: pointer to a counters struct
+ * @param docID: document ID (key)
+ * @param count: count
+ */
 static void
 unite(void* arg, int docID, int count)
 {
@@ -374,12 +452,19 @@ unite(void* arg, int docID, int count)
     int currentCount = counters_get(ctrs, docID);
 
     /* save max to counters */
-    int max;
-    max = (currentCount > count) ? currentCount : count;
-    counters_set(ctrs, docID, max);
+    int total = currentCount + count;
+    counters_set(ctrs, docID, total);
   }
 }
 
+/**
+ * @brief: create a copy of a counters:
+ * Receives key-value pairs and saves them in the counter.
+ * 
+ * @param arg: pointer to a counters
+ * @param docID: document ID (key)
+ * @param count: count
+ */
 static void
 dubCounters(void* arg, int docID, int count)
 {
@@ -392,31 +477,52 @@ dubCounters(void* arg, int docID, int count)
   }
 }
 
+/**
+ * @brief: reset values in counters to zero
+ * 
+ * @param arg: pointer to a valid counters struct
+ * @param docID: document ID (key)
+ * @param count: count
+ */
+static void
+setZero(void* arg, int docID, int count)
+{
+  if(arg != NULL) {
+    /* convert back to counters_t* */
+    counters_t* ctrs = (counters_t*) arg;
+
+    /* insert docID-count pair into the copy */
+    counters_set(ctrs, docID, 0);
+  }
+}
+
+/**
+ * @brief: builds an intersection of two counts.
+ * Compares occurence of keys and values from one counters
+ * with occurrences in the other. 
+ * 
+ * Inputs:
+ * @param arg: array of two counters
+ * @param docID: document ID (key)
+ * @param count: count
+ */
 static void
 intersect(void* arg, int docID, int count) {
   if(arg != NULL) {
     /* convert back to counters_t* */
-    counters_t* intersection = (counters_t*) arg;
+    counters_t** buffer = (counters_t**) arg;
 
-    /* get value in intersection */
-    int currentCount = counters_get(intersection, docID);
+    counters_t* next = buffer[0];
+    counters_t* intersection = buffer[1];
 
-    /* find minimum value of the two */
-    int min = (currentCount < count) ? currentCount : count;
+    int nextCount = counters_get(next, docID);
 
-    /* if min value is greater than zero, save it. */
-    if (min > 0) {
-      printf("ORIGINAL: ");
-      counters_print(intersection, stdout);
-      printf("\n");
+    if (nextCount > 0) {
+      /* find minimum value of the two */
+      int min = (nextCount < count) ? nextCount : count;
 
+      /* save to intersection */
       counters_set(intersection, docID, min);
-
-      printf("UPDATED: ");
-      counters_print(intersection, stdout);
-      printf("\n");
     }
   }
-
-
 }
